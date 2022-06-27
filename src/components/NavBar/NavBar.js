da12aebdc6f29712a7f278a7c878378be3d72d8f
Kortney <kortney@iMac.local>
2022-06-27 09:44:40 -0300

    Styled NavBar component with CSS

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,18 @@
 import React from "react";
 import plane from "../../images/takeoff.svg";
+import NavBarCSS from "./NavBar.css";
 
 export default function NavBar() {
     return (
         <div className="nav">
-            <object data={plane} />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="white"></svg>
+
+            <img
+                src={plane}
+                fill="white"
+                className="nav--image"
+                alt="plane taking off"
+            />
             <h2>my travel journal.</h2>
         </div>
     );
